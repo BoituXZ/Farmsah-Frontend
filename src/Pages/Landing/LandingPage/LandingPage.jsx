1b6ae5d99f9cfbe84573ad93b4bf7e57a868fc5e
Boitumelo <167770627+BoituXZ@users.noreply.github.com>
2024-12-22 22:08:40 +0200

    Refactor landing page layout and styles; enhance responsiveness and visual hierarchy

--- src/Pages/Landing/LandingPage/LandingPage.jsx
+++ src/Pages/Landing/LandingPage/LandingPage.jsx
@@ -5,20 +5,21 @@ const LandingPage = () => {
   return (
     <div className={styles.LPContainer}>
       <section className={styles.heroSection}>
+        <div className={styles.headlineText}>
           <h2>Southern African farmers lost</h2>
           <span>50%</span>
-          <h4>of their crops in 2024 due to unpredictable weather and poor planning.</h4>
-        <div className="headlineText">
-          
+          <h4>of their crops in 2024 due to<br/> unpredictable weather and poor planning.</h4>
+        
         </div>
-        <div className="supportiveHeadline">
-          <h2>We’re here to change that with 
+        <div className={styles.supportiveHeadline}>
+          <h2>We’re here to change that with<br/>
           <span>AI-Powered Farming</span> Tools.</h2>
           <p>Get accurate crop suggestions, real-time weather insights, and smart tools to<br/> boost your yields—no matter the climate challenges.</p>
         </div>
-        <div className="callToAction">
-          <h1>Start Optimizing Your Farm Today
-          See How It Works</h1>
+        <div className={styles.callToAction}>
+          <h1>Start Optimizing Your Farm Today<br/>
+          See How It <span>Works</span></h1>
+          <button>Learn More</button>
         </div>
         </section>
     </div>
