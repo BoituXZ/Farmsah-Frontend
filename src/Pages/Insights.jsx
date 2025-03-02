a2300db00aa2cf62ee863fa520754248b30e69e5
Boitumelo <167770627+BoituXZ@users.noreply.github.com>
2025-03-02 20:35:40 +0200

    Enhance Insights and CropCard components: add multiple InsightCards, improve styling, and ensure page reload after edits and deletions

--- src/Pages/Insights.jsx
+++ src/Pages/Insights.jsx
@@ -17,6 +17,11 @@ const Insights = () => {
       <InsightCard />
       <InsightCard />
       <InsightCard />
+      <InsightCard />
+      <InsightCard />
+      <InsightCard />
+
+
 
     </Box>
   )
